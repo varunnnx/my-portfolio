efa083c0ebce3485d507bc45acc06ba4f0fa0955
varun <varun.nagpal@somaiya.edu>
2025-06-19 23:44:12 +0530

    Fix import case sensitivity for Contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/skills";
 import Experience from "./components/Experience";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 function App() {
   return (
